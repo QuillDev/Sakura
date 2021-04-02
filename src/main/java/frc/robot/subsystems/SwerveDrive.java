6aff2cabade6cf97f45c7a321e0480709680aedc
Robert Brunson <siinspam@gmail.com>
2021-04-02 16:54:32 -0400

    testing inputs

---
+++ src/main/java/frc/robot/subsystems/SwerveDrive.java
@@ -0,0 +1,18 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class SwerveDrive extends SubsystemBase {
+
+    private final double DEADZONE = .15; //Configure the deadzone for input when driving
+
+    public void drive(double fwd, double str, double rcw, boolean fieldOriented){
+
+        //Apply deadzones to the speeds
+        if(Math.abs(fwd) <= DEADZONE) { fwd = 0; }
+        if(Math.abs(str) <= DEADZONE) { str = 0; }
+        if(Math.abs(rcw) <= DEADZONE) { rcw = 0; }
+        
+        System.out.printf("FWD:%s | STR:%s | RCW: %s | FIELD: %s\n", fwd, str, rcw, fieldOriented);
+    }
+}
